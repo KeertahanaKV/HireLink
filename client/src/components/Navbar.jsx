b5f4b3f80f60bb50f8dfa4580ca5c9171d2d383b
Keerthana K V <keerthanakv08@gmail.com>
2025-06-17 01:41:12 +0530

    Initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useClerk,UserButton,useUser } from '@clerk/clerk-react'
+
+const Navbar = () => {
+
+    const {openSignIn}=useClerk()
+  return (
+    <div className="shadow-md py-4 bg-white">
+      <div className="container px-1 2xl:px-20 mx-auto flex justify-between items-center">
+        
+        {/* Logo */}
+        <img src={assets.logo} alt="logo" className="h-10 w-auto" />
+
+        {/* Buttons */}
+        <div className="flex gap-4 max-sm:text-xs">
+          <button className="bg-blue-800 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
+            Recruiter Login
+          </button>
+          <button onClick={e=>openSignIn()} className="border border-blue-600 text-blue-600 px-4 py-2 rounded-lg hover:bg-blue-100 transition">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
